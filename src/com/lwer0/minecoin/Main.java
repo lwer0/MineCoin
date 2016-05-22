ed958fdbdd55a9d5174e14cdfce543493732cb67
Joel <Joel@192.168.1.176>
2016-05-22 23:45:07 +0200

    Plugin Manager, getconnection and get initial coins done.

--- src/com/lwer0/minecoin/Main.java
+++ src/com/lwer0/minecoin/Main.java
@@ -5,6 +5,7 @@
  */
 package com.lwer0.minecoin;
 
+import com.lwer0.minecoin.Listeners.PlayerJoin;
 import com.mysql.jdbc.Connection;
 import java.io.File;
 import java.sql.DriverManager;
@@ -12,6 +13,8 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -20,12 +23,15 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Main extends JavaPlugin {
     
-    private Connection connection;
+    public static Main instance;
+    private static Connection connection;
     private String host, database, username, password;
     private int port;
     
     @Override
     public void onEnable() {
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvents((Listener) new PlayerJoin(instance), this);
         getLogger().info("MineCoin plugin enabled correctly!");
         createConfig();
         FileConfiguration config = this.getConfig();
@@ -70,5 +76,15 @@ public class Main extends JavaPlugin {
             Class.forName("com.mysql.jdbc.Driver");
             connection = (Connection) DriverManager.getConnection("jdbc:mysql://" + this.host+ ":" + this.port + "/" + this.database, this.username, this.password);
         }
+    }
+    
+    public static Connection getConnection() {
+        return connection;
+    }
+    
+    public static int getInCoins() {
+        FileConfiguration config = instance.getConfig();
+        int initialcoins = config.getInt("MineCoins.InitCoins");
+        return initialcoins;
     }
 }
