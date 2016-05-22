85314e0a30a4e7af09351dce0bafd75b1c8bf052
Joel <Joel@192.168.1.176>
2016-05-22 23:41:21 +0200

    API Created

---
+++ src/com/lwer0/minecoin/API.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lwer0.minecoin;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.SQLException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Joel
+ */
+public class API {
+    private final Main plugin;
+    
+    public API (Main instance) {
+        plugin = instance;
+    }
+    
+    public static void CreateProfile(Player p) throws SQLException {
+        if (Main.getConnection() != null) {
+            try {
+                Connection c = Main.getConnection();
+                Statement check = (Statement) c.createStatement();
+                
+                check.executeUpdate("INSERT INTO PlayerData (PLAYERNAME, BALANCE) VALUES ('" + p.getName() + "', " + Main.getInCoins() + ");");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
